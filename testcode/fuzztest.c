ef2e5892c22578a3a5d1a88ce5b6b15212a56a86
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2019-02-06 10:05:38 -0800

    Remove main

--- testcode/fuzztest.c
+++ testcode/fuzztest.c
@@ -32,20 +32,3 @@ int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len)
 		return 1;
 	}
 }
-
-#define BUF_SIZE 65536
-int main(int argc, char** argv)
-{
-    LLVMFuzzerInitialize(&argc, &argv);
-
-    while (__AFL_LOOP(10000)) {
-        uint8_t *buf = malloc(BUF_SIZE);
-        size_t size = read(0, buf, BUF_SIZE);
-
-        LLVMFuzzerTestOneInput(buf, size);
-        free(buf);
-    }
-
-    return 0;
-}
-
