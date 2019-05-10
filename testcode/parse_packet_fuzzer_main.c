5cca178768186d6c92207c95c63822b77a0dec7a
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2019-05-10 12:10:32 -0700

    Fix some build problems.

--- testcode/parse_packet_fuzzer_main.c
+++ testcode/parse_packet_fuzzer_main.c
@@ -9,6 +9,8 @@
 
 #define SZ 10000
 
+int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len);
+
 int main() {
 	char buffer[SZ];
 	size_t n_read = fread(buffer, 1, SZ, stdin);
@@ -16,23 +18,6 @@ int main() {
 		printf("input too big\n");
 		return 1;
 	}
-	sldns_buffer *pkt = sldns_buffer_new(n_read);
-	sldns_buffer_init_frm_data(pkt, buffer, n_read);
 
-	struct regional *region = regional_create();
-
-	struct msg_parse* prs;
-	struct edns_data edns;
-	prs = (struct msg_parse*)malloc(sizeof(struct msg_parse));
-	if(!prs) {
-		printf("out of memory on incoming message\n");
-		return 1;
-	}
-	memset(prs, 0, sizeof(*prs));
-	memset(&edns, 0, sizeof(edns));
-	sldns_buffer_set_position(pkt, 0);
-	if(parse_packet(pkt, prs, region) != LDNS_RCODE_NOERROR) {
-		printf("parse error\n");
-		return 1;
-	}
+	LLVMFuzzerTestOneInput(buffer, n_read);
 }
